158c1da6078c62cd2f8bb000c2befeb035173d13
Arvid Klint <arvidklint@icloud.com>
2021-12-03 20:25:25 +0100

    feat: auto install nvm

--- install.sh
+++ install.sh
@@ -3,5 +3,8 @@
 # install starship prompt
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+
 # link all files
 . ./link.sh
