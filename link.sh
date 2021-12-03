392c2a2306914cd53a250080a7ed6d36ea58261e
Arvid Klint <arvidklint@icloud.com>
2021-12-03 20:24:38 +0100

    feat: update zsh config

--- link.sh
+++ link.sh
@@ -4,10 +4,14 @@ mkdir ~/.config
 
 ln -sv ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 
-# bash/starship
-mkdir ~/.config/bash
-ln -sv ~/dotfiles/bash/.bashrc ~/.bashrc
-ln -sv ~/dotfiles/bash/starship.toml ~/.config/starship.toml
+# starship
+ln -sv ~/dotfiles/starship/starship.toml ~/.config/starship.toml
+
+# bashrc
+# ln -sv ~/dotfiles/bash/.bashrc ~/.bashrc
+
+# zsh
+ln -sv ~/dotfiles/zsh/.zshrc ~/.zshrc
 
 mkdir ~/.config/alacritty
 ln -sv ~/dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
--- macos.sh
+++ macos.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 # Install brew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install ripgrep
+if [[ `uname -m` == 'arm64' ]]; then
+  echo "Installing for M1 chip\n"
+  arch -arm64 brew install ripgrep
+  arch -arm64 brew install neovim
+else
+  brew install ripgrep
+  brew install neovim
+fi
 
 # Fonts
 cp -v ./fonts/dejavu/* ~/Library/Fonts/
