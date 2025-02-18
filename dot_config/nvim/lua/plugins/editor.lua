d1db80ca4f4dbd0435add8cd165f8aad6b8b3876
Arvid Klint <arvidklint@icloud.com>
2025-02-18 09:17:31 +0100

    add file split to fzf-lua

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -6,8 +6,24 @@ return {
         filtered_items = {
           visible = true,
           hide_dotfiles = false,
+          hide_gitignored = false,
         },
       },
     },
+  },
+  {
+    "ibhagwan/fzf-lua",
+    opts = function()
+      local fzf = require("fzf-lua")
+      local actions = fzf.actions
+
+      return {
+        files = {
+          actions = {
+            ["ctrl-o"] = actions.file_vsplit,
+          },
+        },
+      }
+    end,
   },
 }
