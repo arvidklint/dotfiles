f255616c3e5c9cf40101b311e3e16269ded64f0f
Arvid Klint <arvidklint@icloud.com>
2025-02-25 10:50:06 +0100

    nvim: open neotree at cwd

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -10,6 +10,24 @@ return {
         },
       },
     },
+    keys = {
+      {
+        "<leader>fE",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root() })
+        end,
+        desc = "Explorer NeoTree (Root Dir)",
+      },
+      {
+        "<leader>fe",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
+        end,
+        desc = "Explorer NeoTree (cwd)",
+      },
+      { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (cwd)", remap = true },
+      { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (Root Dir)", remap = true },
+    },
   },
   {
     "ibhagwan/fzf-lua",
@@ -26,7 +44,7 @@ return {
       }
     end,
     keys = {
-      { "<c-p>", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
+      { "<c-p>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
     },
   },
 }
