778a31d5bbcad1784574b9d5e717a5f3699c701f
Arvid Klint <arvidklint@icloud.com>
2025-03-13 19:43:02 +0100

    nvim: add tmux navigation

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -41,10 +41,31 @@ return {
             ["ctrl-o"] = actions.file_vsplit,
           },
         },
+        grep = {
+          cwd_prompt = true,
+        },
       }
     end,
     keys = {
       { "<c-p>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
+    },
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
     },
   },
 }
